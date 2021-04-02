e023e3bdbf8180816e07dd9db4e341422d7fab7c
Erdem <hbearstake@yandex.com>
2021-04-02 15:00:53 +0300

    -

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2759,9 +2759,9 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#aa8929",
-    primaryBright: "#aa8929",
-    primaryDark: "#aa8929",
+    primary: "#ffb342",
+    primaryBright: "#ffb342",
+    primaryDark: "#ffb342",
     secondary: "#7645D9",
     success: "#31D0AA",
     warning: "#FFB237",
