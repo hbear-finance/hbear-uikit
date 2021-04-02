ca5db19d40d2e8bd70c0590181558868b025c3b7
Erdem <hbearstake@yandex.com>
2021-04-02 15:54:47 +0300

    -

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -17,7 +17,7 @@ export const brandColors = {
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#2cafff",
+  background: "#FAF9FA",
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
   invertedContrast: "#009eff",
